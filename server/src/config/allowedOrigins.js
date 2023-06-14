4dfc130a37e513854837369bd747925cca6a93a8
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-06-14 09:26:39 +0530

    boilerplate done �

--- server/src/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'http://127.0.0.1:5173/',
-    'http://127.0.0.1:5173',
-    'http://localhost:5000/',
-    'http://localhost:5000'
-]
-
-module.exports = allowedOrigins
