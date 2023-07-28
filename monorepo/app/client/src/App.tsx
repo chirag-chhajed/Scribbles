c76ed1f88d2c276c95684c32cabb4cf51d8721a6
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-28 22:03:10 +0530

    change made

--- monorepo/app/client/src/App.tsx
+++ monorepo/app/client/src/App.tsx
@@ -1,36 +1,55 @@
-import { useEffect,useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+type Message = {
+  name: string;
+  text: string;
+};
 
 function App() {
-  const [count, setCount] = useState<string>("")
-  useEffect(()=>{
-    async function fetchData() {
-      try {
-        const response = await fetch("/api");
-        const text = await response.text();
-        setCount(text);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    void fetchData();
-  },[])
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const socket = io("/api");
+
+  useEffect(() => {
+    socket.emit("findAllChats", {}, (response: Message[]) => {
+      console.log(response);
+      setMessages(response);
+    });
+
+    socket.on('message', (message: Message) => {
+      console.log(message);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    // Clean up the WebSocket listener when the component unmounts
+    return () => {
+      socket.off('message');
+    };
+  }, []);
+
+  // Your rendering and other code goes here...
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 chat">
+        <div className="w-full max-w-lg p-4 border rounded-lg shadow-md chat-container">
+          <div className="px-4 py-2 space-y-2 message-container">
+            {messages.map((message, index) => (
+              <div key={index} className="message">
+                <div className="font-bold text-blue-500 message-name">
+                  {message.name}
+                </div>
+                <div className="p-2 bg-white rounded-lg message-text">
+                  {message.text}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <h1>{count}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
